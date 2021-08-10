95e6772b18ea6963b6d1315dccc82d6981223f0b
Nazia R <nazia.huma@gmail.com>
2021-08-10 16:55:06 +0400

    added save book logic

--- server/resolvers/saveBook.js
+++ server/resolvers/saveBook.js
@@ -1,4 +1,22 @@
-const saveBook = () => {}
+const User = require("../../models/User");
+
+const saveBook = async (_, { input }, context) => {
+  const { id } = context.user;
+
+  // find the user and update the savedBooks field
+  try {
+    return await User.findOneAndUpdate(
+      id,
+      {
+        $push: {
+          savedBooks: input,
+        },
+      },
+      { new: true, runValidators: true }
+    ).populate("savedBooks");
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 module.exports = saveBook;
-

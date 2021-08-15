d1f22f692132fd7f3c14ca5fbbd81f7eb9182a71
Nazia R <nazia.huma@gmail.com>
2021-08-16 00:19:21 +0400

    deleted unwanted files

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:3030/graphql/",
+  //uri: "/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
